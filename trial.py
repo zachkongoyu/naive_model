8334d9c79612a57e5aee961f92a2986addd91def
wouldYouCry <67416202+zachkongoyu@users.noreply.github.com>
2021-08-06 17:53:10 +0800

    Add files via upload

--- trial.py
+++ trial.py
@@ -1,13 +1,41 @@
-import easytrader
+import multiprocessing
+from multiprocessing import Manager
+import requests
+import time
 
-user = easytrader.use('ht_client')
+def foo(targets, return_ls):
+	ans = []
+	for target in targets:
+		headers = {
+				'content-type': 'application/json',
+				'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:22.0) Gecko/20100101 Firefox/22.0'
+			}
+		response = requests.get(f'https://fundgz.1234567.com.cn/js/{target}.js', headers=headers)
+		NAV = float(eval(response.text.replace('jsonpgz(', '').replace(');', ''))['gsz'])
+		ans.append(NAV)
+		
+		return_ls.append(ans)
 
-try:
-	user.prepare(user='666637575127', password='671109', comm_password='671109')
-except:
-	pass
+def get(t):
 
-user.cancel_entrust('20615')
+	targets = [
+		'506002',
+		'501082',
+		'506006',
+		'160926',
+		'506003',
+		'160325',
+		'501080'
+	]
 
-print(user.balance)
-print(user.position)
+	manager = Manager()
+	# return_list = manager.list() 也可以使用列表list
+	return_ls = manager.list()
+
+	p = multiprocessing.Process(target=foo, args=(targets,return_ls))
+	p.start()
+	time.sleep(t)
+	p.terminate()
+	p.join()
+
+	return return_ls.
