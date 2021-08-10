cf7f8e4af6542de2db66735fbb2d5df4056309c9
wouldYouCry <67416202+zachkongoyu@users.noreply.github.com>
2021-08-10 10:04:13 +0800

    Add files via upload

--- scrape.py
+++ scrape.py
@@ -1,41 +1,53 @@
-from liveEvaluation import stream
+import datetime
+import pandas as pd
+import requests
 import time
 
-chinaamc = stream(160325)
-fullgoal = stream(161040)
-bosera = stream(501082)
-efund = stream(506002)
+def scrape(interval):
+	
+	targets = [
+			'160325',
+			'160926',
+			'501080',
+			'501082',
+			'506002',
+			'506003',
+			'506006'
+		]
+	
+	log_df = pd.DataFrame()
+	
+	dt = datetime.datetime.now()
+	
+	while dt.time() < datetime.time(11, 30):
+	
+		print(dt.time())
+		now = datetime.datetime.now()
 
-def scrape(interval, times):
-
-	now=time.time()
-	cur_times = 0
-
-	while cur_times != times:
-
-		end = time.time()
-
-		chinaamc.get_evaluation()
-		chinaamc.to_df()
-		chinaamc.df.to_csv('160325_NAV.csv')
-
-		fullgoal.get_evaluation()
-		fullgoal.to_df()
-		fullgoal.df.to_csv('161040_NAV.csv')
-
-		bosera.get_evaluation()
-		bosera.to_df()
-		bosera.df.to_csv('501082_NAV.csv')
-
-		efund.get_evaluation()
-		efund.to_df()
-		efund.df.to_csv('506002_NAV.csv')
-
-		cur_times += 1
-
+		snapshot = {now : {}}
+		for code in targets:
+			
+			headers = {
+				'content-type': 'application/json',
+				'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:22.0) Gecko/20100101 Firefox/22.0'
+			}
+			
+			response = requests.get(f'https://fundgz.1234567.com.cn/js/{code}.js', headers=headers)
+			NAV = float(eval(response.text.replace('jsonpgz(', '').replace(');', ''))['gsz'])
+			
+			snapshot[now][code + ' NAV'] = NAV
+		
+		dt = datetime.datetime.now()
+		
+		log_df = pd.concat([log_df, pd.DataFrame(snapshot).T], axis=0)
+	
 		time.sleep(interval)
+		
+	return log_df
 
 
 if __name__ == '__main__':
 
-	scrape(0, 9)
+	log_df = scrape(60)
+
+	log_df.to_csv('test.csv')
